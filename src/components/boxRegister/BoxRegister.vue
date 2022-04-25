788ec769bce021b8fa81ed5cd1f9dc6c47c700db
Juansantoss07 <juansantosscunha@gmail.com>
2022-04-25 14:49:24 -0300

    add view register

---
+++ src/components/boxRegister/BoxRegister.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: "BoxRegister",
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="back">
+      <a href="#"><p>Voltar para o login</p></a>
+    </div>
+
+    <div class="box-register">
+      <h3>Dados pessoais</h3>
+      <form action="">
+        <div class="input">
+          <label for="">Nome completo</label>
+          <input type="text" name="" id="" placeholder="Seu nome" />
+        </div>
+        <div class="input">
+          <label for="">E-mail</label>
+          <input type="email" name="" id="" placeholder="nome@mail.com" />
+        </div>
+        <div class="input">
+          <label for="">Senha</label>
+          <input type="password" name="" id="" placeholder="* * * *" />
+        </div>
+        <div class="input">
+          <input type="file" name="" id="" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./boxregister.scss" scoped/>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,69 @@
 <script>
+import BoxTerms from "../components/boxTerms/BoxTerms.vue";
+import Banner from "../components/banner/Banner.vue";
+import BoxRegister from "../components/boxRegister/BoxRegister.vue";
+
 export default {
   name: "Register",
+
+  components: {
+    BoxTerms,
+    Banner,
+    BoxRegister,
+  },
 };
-</script>
+</script> 
 
 <template>
-  <h1>Register</h1>
+  <div class="content-register">
+    <div class="box-left">
+      <BoxRegister />
+    </div>
+    <div class="box-right">
+      <Banner />
+      <BoxTerms />
+    </div>
+  </div>
 </template>
 
+<style lang="scss" scoped>
+.content-register {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  .box-right {
+    width: 40%;
+    height: 95vh;
+    display: flex;
+    flex-direction: column;
+    margin: 1rem;
+    justify-content: space-between;
+  }
+
+  .box-left {
+    width: 55%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+@media (max-width: 812px) {
+  .content-register {
+    flex-direction: column;
+
+    .box-left,
+    .box-right {
+      width: 100%;
+    }
+
+    .box-right {
+      margin: 0 !important;
+    }
+
+    .box-left {
+      height: 100vh;
+    }
+  }
+}
+</style>
